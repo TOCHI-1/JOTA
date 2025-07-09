69415a8a87442ed54031dece2737942f09eb6bf6
Tochi Anyogu <tochianyogu@gmail.com>
2025-07-09 21:17:52 +0100

    for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   darkMode: false,  
+  base: '/JOTA/',
+    build: {
+    outDir: 'docs'              
+  },
+
   plugins: [
     react(),
     tailwindcss(),
